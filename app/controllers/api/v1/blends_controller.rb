abdaa33b1dce07aed5d2815e685157aa8e7d17c3
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-01-02 11:24:33 -0500

    draft show blend feature

--- app/controllers/api/v1/blends_controller.rb
+++ app/controllers/api/v1/blends_controller.rb
@@ -1,8 +1,21 @@
 class Api::V1::BlendsController < ApplicationController
+  before_action :set_blend, only: [:show,:update,:destroy]
 
   def index
     @blends = Blend.all
     render json: @blends
   end
 
+  def show
+    render json: @blend, status: 200
+  end
+
+  private
+  def blend_params
+    params.permit(:name, :origin, :variety)
+  end
+
+  def set_blend
+    @blend = Blend.find(params[:id])
+  end
 end
